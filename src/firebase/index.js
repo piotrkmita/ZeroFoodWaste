e5ead472fef280829d064d52b121c7de8103e442
DrazekKD <drazek3850@gmail.com>
2020-05-02 14:59:30 +0200

    Appv1 (#13)
    * initial project
    
    * add plugins style
    
    * add firebase settings
    
    * add all assets in app
    
    * add router app
    
    * add store function
    
    * add app navigation
    
    * add about app views
    
    Co-authored-by: Kamil Drążkiewicz <k.drazkiewicz@getprintbox.com>

---
+++ src/firebase/index.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase';
+
+const config = {
+	apiKey: "AIzaSyCY9hXsW9rCKAS6VJnwZahZjxjiftAXGIA",
+	authDomain: "bai-studia.firebaseapp.com",
+	databaseURL: "https://bai-studia.firebaseio.com",
+	projectId: "bai-studia",
+	storageBucket: "bai-studia.appspot.com",
+	messagingSenderId: "812238190690",
+	appId: "1:812238190690:web:9edea56ef32fd6fb32393b"
+};
+firebase.initializeApp(config);
