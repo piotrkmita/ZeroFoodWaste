1e777415f9cd76cd2aeed9144d0eff0dd399012e
Piotr Kmita <piotr.kmita@zalando.de>
2020-05-02 20:13:13 +0200

    add home hero

---
+++ src/components/HomeHero.vue
@@ -0,0 +1,27 @@
+<template>
+    <v-container fluid fill-height class="home-hero" style="max-height: 100vh;">
+        <v-layout justify-center align-center column pa-5>
+            <div class="display-4 font-weight-black white--text text-xs-center">
+                NIE TRAĆ JEDZENIA.
+            </div>
+            <div class="display-1 font-weight-bold white--text text-xs-center">
+                Wyszukaj przepisów na podstawie składników które masz w lodówce.
+            </div>
+        </v-layout>
+    </v-container>
+</template>
+
+<script>
+export default {
+    name: 'HomeHero'
+};
+</script>
+
+<style scoped>
+.home-hero {
+    background: url('http://source.unsplash.com/0BhSKStVtdM');
+    background-size: cover;
+    width: 100%;
+    height: 100%;
+}
+</style>
